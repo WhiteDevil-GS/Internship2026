699036c3b6297ef787ca7189fd6299c67d9e979e
WhiteDevil-GS <128776329+WhiteDevil-GS@users.noreply.github.com>
2026-03-17 12:34:34 +0530

    Added my project files

---
+++ react-router-app/src/components/Footer.jsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+    return <h4>© 2026 My App</h4>;
+  };
+  
+  export default Footer;
---
+++ react-router-app/src/routes/AppRoutes.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import NotFound from "../pages/NotFound";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default AppRoutes;
