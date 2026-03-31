5f0cf49ac97abb28bb354abe7ce76a05a682089a
Ganesh Soppin <128776329+WhiteDevil-GS@users.noreply.github.com>
2026-03-31 14:20:02 +0530

    Add files via upload

---
+++ hello-server/server.js
@@ -0,0 +1,34 @@
+const http = require('http');
+
+// Create server
+const server = http.createServer((req, res) => {
+
+    // Home route
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome to Home Page');
+    } 
+    
+    // About route
+    else if (req.url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('This is About Page');
+    } 
+    
+    // Contact route
+    else if (req.url === '/contact') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Contact us at example@gmail.com');
+    } 
+    
+    // 404 route
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Page Not Found');
+    }
+});
+
+// Start server
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
